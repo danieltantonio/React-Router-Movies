7cd4148b22718d8300467e3d6df14a97aa7d392e
Daniel Antonio <danieltomantonio@gamil.com>
2020-07-20 12:44:11 -0700

    Created route

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -6,7 +6,7 @@ const Movie = (props) => {
   const [movie, setMovie] = useState();
   const { movieId } = useParams();
   useEffect(() => {
-    const id = movieId;
+    const id = movieId; // Added movieId
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
